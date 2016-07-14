bff72a36d8bf91aa90d95cbc1da6ea20ada03dd3
Maxim Uvarov <40-02@users.noreply.github.com>
2016-07-14 17:48:57 +0300

    Update PQK50Tracker.m

--- PQK50Tracker.m
+++ PQK50Tracker.m
@@ -6,7 +6,7 @@ let
 
     url = "http://api-tracker.k50.ru/api/call/ext/" & id & "/" & From & "/" & To & "?apiKey=" & token,
     Source = Json.Document(Web.Contents(url)),
-    #"Converted to Table" = Table.FromList(Source, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
-    #"Expanded Column1" = Table.ExpandRecordColumn(#"Converted to Table", "Column1", {"id", "start_time", "stop_time", "caller_phone", "called_phone", "duration", "count_call", "is_matching", "sid", "uuid", "utm_source", "utm_medium", "utm_term", "utm_content", "utm_campaign", "type", "analytics_client_id", "entryPoints", "user_agent"}, {"id", "start_time", "stop_time", "caller_phone", "called_phone", "duration", "count_call", "is_matching", "sid", "uuid", "utm_source", "utm_medium", "utm_term", "utm_content", "utm_campaign", "type", "analytics_client_id", "entryPoints", "user_agent"})
+    #"Converted to Table1" = Record.ToTable(Source),
+    #"Expanded Value" = Table.ExpandRecordColumn(#"Converted to Table1", "Value", {"id", "start_time", "stop_time", "caller_phone", "called_phone", "duration", "count_call", "is_matching", "sid", "uuid", "user_agent", "label", "source", "source_type", "keyword", "utm_source", "utm_medium", "utm_term", "utm_content", "utm_campaign", "type", "analytics_client_id", "entry_point", "referrer", "entryPoints"}, {"id", "start_time", "stop_time", "caller_phone", "called_phone", "duration", "count_call", "is_matching", "sid", "uuid", "user_agent", "label", "source", "source_type", "keyword", "utm_source", "utm_medium", "utm_term", "utm_content", "utm_campaign", "type", "analytics_client_id", "entry_point", "referrer", "entryPoints"})
 in
-    #"Expanded Column1"
+    #"Expanded Value"
